af9f005ab71e6b72b70610cc4007e9fb323bcf5d
mihir kapur <mihirkapur96@gmail.com>
2021-02-05 12:57:26 -0800

    Exercise 1

--- js/ex1.js
+++ js/ex1.js
@@ -3,3 +3,15 @@
 */
 
 console.log("Mihir's Output from Homework 3 Exercise 1");
+let name = prompt("Enter your name:");
+   console.log(`Hello, ${name}`);
+let units = Number(prompt("Enter number of units completed"));
+if (30>=units && units>0) {
+   console.log("Your Grade Standing is Freshman");
+} else if (60>=units && units>=31) {
+   console.log("Your Grade Standing is Sophomore");
+} else if (90>=units && units>=61) {
+   console.log("Your Grade Standing is Junior");
+} else {
+   console.log("Your Grade Standing is Senior");
+}
