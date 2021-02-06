21bc32a4062d3dc6158fb617dc21997bbcc3762e
mihir kapur <mihirkapur96@gmail.com>
2021-02-05 17:52:11 -0800

    Second Exercise

--- js/ex2.js
+++ js/ex2.js
@@ -3,3 +3,29 @@
 */
 
 console.log("Mihir's Output from Homework 3 Exercise 2");
+let day= prompt("Enter the day");
+switch (day) {
+case "sun" :
+   console.log("The following day is Sunday");
+   break;
+case "mon" :
+   console.log("The following day is Monday");
+   break;
+case "tue" :
+   console.log("The following day is Tuesday");
+   break;
+case "wed" :
+   console.log("The following day is Wednesday");
+   break;
+case "thu" :
+   console.log("The following day is Thursday");
+   break;
+case "fri" :
+   console.log("The following day is Friday");
+   break;
+case "sat" :
+   console.log("The following day is Saturday");
+   break;
+   default:
+      console.log("Not a valid day type");
+}
