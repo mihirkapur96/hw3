5cdee732039e47b2116e2309772068a67f28f1a6
mihir kapur <mihirkapur96@gmail.com>
2021-02-07 15:58:12 -0800

    Exercise 5

--- js/ex5.js
+++ js/ex5.js
@@ -3,3 +3,9 @@
 */
 
 console.log("Mihir's Output from Homework 3 Exercise 5");
+let number= prompt ("Enter a number");
+for (let i = 0; i < 11; i++) {
+   if (number = number) {
+   console.log(`${number}*${i} = ${number*i}`);   
+   }
+}
