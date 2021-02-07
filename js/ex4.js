ec3107bc7d2f170335f559cf226fe65c940c7072
mihir kapur <mihirkapur96@gmail.com>
2021-02-07 15:17:24 -0800

    Exercise 4

--- js/ex4.js
+++ js/ex4.js
@@ -3,3 +3,18 @@
 */
 
 console.log("Mihir's Output from Homework 3 Exercise 4");
+let test = ""
+for (let i=1 ; i <= 3; i++) {
+   test= prompt("Enter the Password");
+   if( test == "secret") {
+      console.log (`You have entered the correct password after ${i} attempts`);
+      break;
+   } else if ( test !== "secret"){
+      console.log (" Enter the Password again");
+   }
+
+   if (i == 3) {
+      console.log (`Your Account is locked! You failed to enter the correct password ${i} attempts`);
+   }
+}
+
