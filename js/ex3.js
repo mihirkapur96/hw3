5f5dc8a842750c9e7fb4ad86a35e3c1a076d0d3f
mihir kapur <mihirkapur96@gmail.com>
2021-02-07 20:45:46 -0800

    Exercise 6

--- js/ex3.js
+++ js/ex3.js
@@ -5,7 +5,7 @@
 console.log("Mihir's Output from Homework 3 Exercise 3");
 let num1 = Number(prompt("Enter the first number"));
 let num2 = Number(prompt("Enter the second number"));
-if (num1>0 && num2 >0) {
+if (num1 > 0 && num2 > 0) {
    console.log(`Addition = ${num1+num2}`);
    console.log(`Subtraction = ${num1-num2}`);
    console.log(`Multiplication = ${num1*num2}`);
--- js/ex6.js
+++ js/ex6.js
@@ -3,3 +3,36 @@
 */
 
 console.log("Mihir's Output from Homework 3 Exercise 6");
+let hours = Number(prompt("Enter the hour value"));
+let minutes = Number(prompt("Enter the minutes value"));
+let seconds = Number(prompt("Enter the seconds value"));
+
+// validation
+if (hours >= 24 ) {
+   console.log("Invalid Hour Input");
+}
+if (minutes > 60){
+   console.log ("Invalid Minutes Input");
+}
+if (seconds > 60){
+   console.log ("Invalid Seconds Input");
+}
+// after validation
+if (hours <= 23 && minutes <= 59 && seconds <= 59) {
+   console.log(`Time input:${hours}:${minutes}:${seconds}`);
+   seconds++;
+   if (seconds==60) {
+      seconds = 0;
+      minutes++;
+   }
+
+   if (minutes == 60) {
+      minutes = 0;
+      hours++;
+   }
+
+   if (hours == 24) {
+      hours = 0;
+   }
+   console.log(`One second later:${hours}h:${minutes}m:${seconds}s`);
+}
