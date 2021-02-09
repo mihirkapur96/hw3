16e957d798ae8bb5331793d24a5f8bc2e54b507d
mihir kapur <mihirkapur96@gmail.com>
2021-02-08 16:45:38 -0800

    Exercise 2 correction

--- js/ex2.js
+++ js/ex2.js
@@ -6,25 +6,32 @@ console.log("Mihir's Output from Homework 3 Exercise 2");
 let day= prompt("Enter the day");
 switch (day) {
 case "sun" :
-   console.log("The following day is: Sunday");
-   break;
-case "mon" :
+   console.log("You entered: Sunday");
    console.log("The following day is: Monday");
    break;
-case "tue" :
+case "mon" :
+   console.log("You entered: Monday");
    console.log("The following day is: Tuesday");
    break;
-case "wed" :
+case "tue" :
+   console.log("You entered: Tuesday");
    console.log("The following day is: Wednesday");
    break;
-case "thu" :
+case "wed" :
+   console.log("You entered: Wednesday");
    console.log("The following day is: Thursday");
    break;
-case "fri" :
+case "thu" :
+   console.log("You entered: Thursday");
    console.log("The following day is: Friday");
    break;
-case "sat" :
+case "fri" :
+   console.log("You entered: Friday");
    console.log("The following day is: Saturday");
+   break;
+case "sat" :
+   console.log("You entered: Saturday");
+   console.log("The following day is: Sunday");
    break;
    default:
       console.log("Not a valid day type");
